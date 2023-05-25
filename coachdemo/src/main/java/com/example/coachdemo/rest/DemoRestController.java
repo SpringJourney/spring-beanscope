ddc52fd3f1662b42096e7c29888f83ea254d59c9
Yao Wang <dell9459@gmail.com>
2023-05-24 23:44:49 -0400

    adding global lazy initialization and SCOPE_PROTOTYPE

--- coachdemo/src/main/java/com/example/coachdemo/rest/DemoRestController.java
+++ coachdemo/src/main/java/com/example/coachdemo/rest/DemoRestController.java
@@ -9,19 +9,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoRestController {
     private Coach myCoach;
+    private Coach antoherCoach;
 
 //    @Autowired
 //    public DemoRestController(final Coach theCoach) {
 //        myCoach = theCoach;
 //    }
     @Autowired
-    public DemoRestController(@Qualifier("baseballCoach") Coach theCoach) {
+    public DemoRestController(@Qualifier("trackCoach")Coach theCoach,@Qualifier("trackCoach") Coach otherCoach ) {
         myCoach = theCoach;
+        antoherCoach = otherCoach;
     }
 
     @GetMapping("/dailyworkout")
     public String getDailyWorkout() {
         return myCoach.getDailyWorkout();
     }
-
+    @GetMapping("/check")
+    public String check() {
+        return "Comparing theCoach and otherCoach <br />["+ myCoach.getClass() + "]<br /> vs\n[" + antoherCoach.getClass() + "] , the result is " + (myCoach == antoherCoach);
+    }
 }
